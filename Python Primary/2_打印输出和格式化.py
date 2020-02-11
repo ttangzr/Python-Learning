2f898cb2953636a40c66a56bf88ac97783087cf4
ttangzr <ttangzr@163.com>
2020-02-11 09:53:59 +0800

    Update Python Primary/2_.py

---
+++ Python Primary/2_打印输出和格式化.py
@@ -0,0 +1,14 @@
+def func_2_3():
+    # 分隔符打印多个字符串
+    str0 = "123"
+    str1 = "456%s" # 字符串中的%s不会被解释为格式化字符串
+
+    # 含有%s的格式化字符串只能有一个，且位于最后
+    print(str0,str1,"%s%s" %("end", "!"))
+    print(str0,str1,"%s%s" %("end", "!"), sep='*')
+
+    print("%s*%s*end!" % (str0, str1)) # 手动指定分隔符
+    
+
+if __name__ = "__main__":
+    func_2_3()
